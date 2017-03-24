daab9ece6e885a6b86412251e2b79d7be5694a4b
DovidM <landyx47@gmail.com>
2017-03-24 11:13:31 -0400

    Fixed bug where sub-bullets didn't display properly, added support for sub and subscripts

--- vimToGoogleDocs.js
+++ vimToGoogleDocs.js
@@ -11,11 +11,17 @@ var fs = require("fs");
 
        var spaces = "";
 
-       for (var i = 0; i < 8; i++) {
+       for (var i = 0; i < 12; i++) {
 	       spaces += "&nbsp;";
        };
 
-       var newValue = data.replace(/(.+)\n/gi, "<li>$1</li>\n").replace(/\n\n/gi, "<br />").replace(/<li>\t(.*)<\/li>/gi, spaces + "o $1").replace(/(Q|A\d?):/gi, "<em>$1</em>:").replace(/\:\s([a-zA-Z\'"\s]+):/gi, ": <u>$1</u>:");
+       var newValue = data.replace(/(.+)\n/gi, "<ul><li>$1</li></ul>\n")
+	                  .replace(/\n\n/gi, "<br />")
+			  .replace(/<ul><li>\t(.*)<\/li><\/ul>/gi, "<div>" + spaces + "o $1</div>")
+			  .replace(/(Q|A\d?):/gi, "<em>$1</em>:")
+			  .replace(/\:\s([a-zA-Z\'"\s]+):/gi, ": <u>$1</u>:")
+			  .replace(/\^(\w+)/gi, "<sup>$1</sup>")
+			  .replace(/§(\w+)/gi, "<sub>$1</sub>");
 
        newValue = '<font size="12pt">' + newValue + '</font>'; 
 
